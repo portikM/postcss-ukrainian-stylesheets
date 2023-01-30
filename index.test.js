41550a0101ae8c9079b7e7e7fe92c2f4671d977f
portikM <portimaksym@gmail.com>
2023-01-29 23:39:58 -0500

    test: add sample test

--- index.test.js
+++ index.test.js
@@ -2,16 +2,14 @@ const postcss = require('postcss')
 
 const plugin = require('./')
 
+// eslint-disable-next-line no-unused-vars
 async function run (input, output, opts = { }) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
   expect(result.css).toEqual(output)
   expect(result.warnings()).toHaveLength(0)
 }
 
-/* Write tests here
-
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+// keep it simple for now to make sure the CI works
+it('does something', () => {
+  expect(1).toBe(1);
 })
-
-*/
