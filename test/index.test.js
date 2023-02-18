76654873000d0dcde581b17c5fc0c95a2a22fe02
portikM <portimaksym@gmail.com>
2023-02-18 11:54:35 -0500

    test: add tests

--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,9 @@ const postcss = require('postcss')
 
 const plugin = require('../src')
 
+const UAProperties = require('../src/properties')
+const UAValues = require('../src/values')
+
 // eslint-disable-next-line no-unused-vars
 async function run (input, output, opts = { }) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
@@ -9,7 +12,29 @@ async function run (input, output, opts = { }) {
   expect(result.warnings()).toHaveLength(0)
 }
 
-// keep it simple for now to make sure the CI works
-it('does something', () => {
-  expect(1).toBe(1);
+const getRandomValue = () => {
+  // return random ukrainian values key: value pair
+  const valueIndex = Math.floor(Math.random() * Object.entries(UAValues).length) + 1
+  return { vKey: Object.keys(UAValues)[valueIndex], vValue: Object.values(UAValues)[valueIndex] } 
+}
+
+describe('postcss-ukrainian-stylesheets', () => {
+  it('converts ukrainian properties to english', () => {
+    for (const [key, value] of Object.entries(UAProperties)) {
+      run(`span{${value}: none;}`, `span{${key}: none;}`)
+    }
+  })
+
+  it('converts ukrainian values to english', () => {
+    for (const [key, value] of Object.entries(UAValues)) {
+      run(`span{background-color: ${value};}`, `span{background-color: ${key};}`)
+    }
+  })
+
+  it('converts ukrainian property: value !важливо to english property: value !important', () => {
+    for (const [pKey, pValue] of Object.entries(UAProperties)) {
+      const { vKey, vValue } = getRandomValue()
+      run(`span{${pValue}: ${vValue} !важливо;}`, `span{${pKey}: ${vKey} !important;}`)
+    }
+  })
 })
